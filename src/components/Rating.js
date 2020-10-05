30a3aaca12a9a714a67b5fdbcc5ec87a9cb540a0
NoobSaiyan <ms19961998@gmail.com>
2020-10-05 23:14:03 +0530

    stars rating

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -2,9 +2,13 @@ import React from 'react'
 import '../css/rating.css'
 
 const Rating = ({ justify, align, name }) => {
+  let names = ['icon-star', 'icon-star', 'icon-star', 'icon-star', 'icon-star']
+  const stars = names.map((name, index) => (
+    <img key={index} src={`/images/${name}.svg`} alt='' />
+  ))
   return (
     <div className='card' style={{ justifySelf: justify, alignSelf: align }}>
-      <div className='stars'></div>
+      <div className='stars'>{stars}</div>
       <div className='review'>Rated 5 Stars in {name}</div>
     </div>
   )
