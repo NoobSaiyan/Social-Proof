71cd5cba2a945420987e210768485c1ba9dd55cc
NoobSaiyan <ms19961998@gmail.com>
2020-10-06 11:28:07 +0530

    added spaces b/w stars

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -4,11 +4,14 @@ import '../css/rating.css'
 const Rating = ({ justify, align, name }) => {
   let names = ['icon-star', 'icon-star', 'icon-star', 'icon-star', 'icon-star']
   const stars = names.map((name, index) => (
-    <img key={index} src={`/images/${name}.svg`} alt='' />
+    <div key={index} style={{ display: 'inline' }}>
+      {' '}
+      <img key={index} src={`/images/${name}.svg`} alt='' />
+    </div>
   ))
   return (
     <div className='card' style={{ justifySelf: justify, alignSelf: align }}>
-      <div className='stars'>{stars}</div>
+      <div className='stars'>{stars} </div>
       <div className='review'>Rated 5 Stars in {name}</div>
     </div>
   )
